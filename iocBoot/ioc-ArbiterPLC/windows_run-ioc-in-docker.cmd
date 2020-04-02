c8cebd0bd1f593b4f309d16ea4bb9fdd055fde4b
Alex Wallace <awallace@slac.stanford.edu>
2020-04-02 08:32:09 -0700

    Rebuild after removal

---
+++ iocBoot/ioc-ArbiterPLC/windows_run-ioc-in-docker.cmd
@@ -0,0 +1,15 @@
+@echo off
+echo Your development environment Net ID is: 172.21.88.75.1.1
+echo.
+echo You must fully exit TwinCAT for this IOC to work.
+echo Please close TwinCAT now and
+pause
+echo on
+
+"C:/Program Files/Docker/Docker/resources/bin/docker.exe" run ^
+        -v "c:\Repos\ads-deploy\tools\:/ads-deploy/tools" ^
+        -v "C:\Users\awallace\Documents\Source\pmps\pmps-dev-arbiter\:/reg/g/pcds/epics/ioc/Arbiter" ^
+	-e DISPLAY=host.docker.internal:0.0 ^
+	-i pcdshub/ads-deploy:latest ^
+        "make -C ${ADS_IOC_PATH}/iocBoot/templates && cd '/reg/g/pcds/epics/ioc/Arbiter/iocBoot/ioc-ArbiterPLC' && make && sed -i '/^adsIoc_registerRecord.*$/a adsSetLocalAddress(172.21.88.75.1.1)' st.cmd && ./st.cmd; echo 'IOC exited.'; sleep 1"
+pause
